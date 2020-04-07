ee9156f269cda982bd38fb1d0617b6fdb64056e0
Mike Galvin <digressive@outlook.com>
2020-04-07 19:41:09 +0100

    Create What-Day-Is-It-Teams.ps1

---
+++ PowerShell-to-Teams/What-Day-Is-It-Teams.ps1
@@ -0,0 +1,24 @@
+﻿$TDay = (Get-Date).DayOfWeek
+
+$uri = "https://outlook.office.com/webhook/put-your-webhook-here"
+
+$ResultArr = @()
+
+$ResultArr += New-Object PSObject -Property @{
+    facts = @(
+    @{
+        name = 'Today is:'
+        value = "$TDay"
+    })
+}
+
+If ($Null -ne $ResultArr)
+{
+    $Body = ConvertTo-Json -Depth 8 @{
+    text  = " "
+    sections = $ResultArr
+    title = "What day of the week is it?"
+    }
+
+    Invoke-RestMethod -Uri $Uri -Method Post -body $Body -ContentType 'application/json'
+}
