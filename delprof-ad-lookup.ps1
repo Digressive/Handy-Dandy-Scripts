a068726c43e9329100f36b8e042216578fc324e0
Digressive <40495197+Digressive@users.noreply.github.com>
2021-06-15 16:33:41 +0100

    Create delprof-ad-lookup.ps1

---
+++ delprof-ad-lookup.ps1
@@ -0,0 +1,23 @@
+﻿$PCList = "PC1,PC2"
+$PCs = $PCList.split(",")
+
+ForEach ($PC in $PCs)
+{	
+	$UsrList = Get-ChildItem "\\$PC\C$\Users" -Name -Directory -Exclude 'Administrator','Public','sysadmin'
+
+    ForEach ($User in $UsrList)
+    {
+		$UsrExist = Get-ADUser -Filter 'Name -like "$User"'
+
+        If ($UsrExist -eq $null)
+        {
+            Write-host "User:$User not found in AD"
+			Get-CimInstance -ComputerName "$PC" -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -eq $User } | Remove-CimInstance
+        }
+		
+		else{
+			Write-host "Goodbye"
+		}
+		
+    }
+}
